6ad9a5cc16451cf95bfb05987bea4d252db66615
Charl de Waal <charldewaal8@gmail.com>
2022-07-03 20:36:56 +0200

    Added async thunks for fetching categories and all apis

--- src/features/apiSlice.js
+++ src/features/apiSlice.js
@@ -1,12 +1,24 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchRandomAPI } from "../utils/utils";
+import { fetchRandomAPI, fetchAPICategories, fetchAllAPIs } from "../utils/utils";
 
 export const fetchRandom = createAsyncThunk('api/fetchRandom', async () => {
     const res = await fetchRandomAPI();
     return res.entries[0];
 });
 
+export const fetchCategories = createAsyncThunk('api/fetchCategories', async () => {
+    const res = await fetchAPICategories();
+    return res.categories;
+});
+
+export const fetchAll = createAsyncThunk('api/fetchAll', async () => {
+    const res = await fetchAllAPIs();
+    return res.entries;
+});
+
 const initialState = {
+    allApis: [],
+    categories: [],
     randomApi: [],
     isLoading: false
 };
@@ -16,6 +28,13 @@ export const apiSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers: {
+        [fetchCategories.pending]: (state) => {
+            state.isLoading = false;
+        },
+        [fetchCategories.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.categories = action.payload;
+        },
         [fetchRandom.pending]: (state) => {
             state.isLoading = true;
         },
@@ -27,5 +46,6 @@ export const apiSlice = createSlice({
 });
 
 export const selectRandomApi = (state) => state.api.randomApi;
+export const selectCategories = (state) => state.api.categories;
 export const isLoading = (state) => state.api.isLoading;
 export default apiSlice.reducer; 
