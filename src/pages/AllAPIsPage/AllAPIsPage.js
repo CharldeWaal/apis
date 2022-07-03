ecdf69be70e14bf34ac1f775a399ce841eab3c9b
Charl de Waal <charldewaal8@gmail.com>
2022-07-03 20:48:08 +0200

    Refactored page to use Redux

--- src/pages/AllAPIsPage/AllAPIsPage.js
+++ src/pages/AllAPIsPage/AllAPIsPage.js
@@ -1,23 +1,24 @@
 import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
 
-import { fetchAllAPIs } from "../../utils/utils";
+//import redux selectors, dispatch
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAll, isLoading, selectAllAPIs } from "../../features/apiSlice";
 
 export const AllAPIsPage = () => {
-    const [apis, setApis] = useState([]);
+    const dispatch = useDispatch();
+    const allAPIs = useSelector(selectAllAPIs);
+    const loading = useSelector(isLoading);
 
     useEffect(() => {
-        fetchAllAPIs().then(res => {
-            console.log(res.entries);
-            setApis(res.entries);
-        })
+        dispatch(fetchAll());
     }, [])
 
     return (
         <div>
             <ul>
-                {apis.length > 0 &&
-                apis.map((data, index) => {
+                {loading ? 'Loading...' :
+                allAPIs.map((data, index) => {
                     return (
                         <li key={index}>{data.API}</li> 
                     )
