ae60568dbebb862e2f1f020f28022fff2ac9216e
Charl de Waal <charldewaal8@gmail.com>
2022-07-04 19:36:32 +0200

    Replaced list with styled table

--- src/pages/AllAPIsPage/AllAPIsPage.js
+++ src/pages/AllAPIsPage/AllAPIsPage.js
@@ -5,6 +5,37 @@ import styled from 'styled-components';
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAll, isLoading, selectAllAPIs } from "../../features/apiSlice";
 
+const StyledTable = styled.table`
+    border: 1px solid #48434f;
+    border-collapse: collapse;
+`;
+
+const TableRow = styled.tr`
+    border: 1px solid #48434f;
+
+    &:nth-child(even) {background-color: #f2f2f2;}
+`;
+
+const TableHeader = styled.th`
+    padding: 16px;
+    border: 1px solid #48434f;
+    text-align: left;
+`; 
+
+const TableData = styled.td`
+    padding: 16px;
+    border: 1px solid #48434f;
+    text-align: left;
+`;
+
+const Link = styled.a`
+    text-decoration: none;
+
+    &:hover{
+        text-decoration: underline;
+    }
+`;
+
 export const AllAPIsPage = () => {
     const dispatch = useDispatch();
     const allAPIs = useSelector(selectAllAPIs);
@@ -16,14 +47,31 @@ export const AllAPIsPage = () => {
 
     return (
         <div>
-            <ul>
-                {loading ? 'Loading...' :
-                allAPIs.map((data, index) => {
+            <StyledTable>
+                <thead>
+                    <TableRow>
+                        <TableHeader>API</TableHeader>
+                        <TableHeader>Description</TableHeader>
+                        <TableHeader>Auth</TableHeader>
+                        <TableHeader>HTTPS</TableHeader>
+                        <TableHeader>CORS</TableHeader>
+                    </TableRow>
+                </thead>
+                <tbody>
+                    {loading ? 'Loading...' :
+                    allAPIs.map((data, index) => {
                     return (
-                        <li key={index}>{data.API}</li> 
-                    )
-                })}
-            </ul>
+                        <TableRow key={index}>
+                            <TableData><Link href={data.Link} target='_blank'>{data.API}</Link></TableData>
+                            <TableData>{data.Description}</TableData>
+                            <TableData>{data.Auth ? data.Auth : 'No'}</TableData>
+                            <TableData>{data.HTTPS === true ? 'Yes' : 'No'}</TableData>
+                            <TableData>{data.Cors.charAt(0).toUpperCase() + data.Cors.substring(1)}</TableData>
+                        </TableRow>
+                        )
+                    })}
+                </tbody>
+            </StyledTable>
         </div>
     )
 }
