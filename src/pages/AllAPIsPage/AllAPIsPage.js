ef0aa2465480d4ec126b1b48ff9dc36355bb5d94
Charl de Waal <charldewaal8@gmail.com>
2022-07-05 17:38:33 +0200

    Refactored error message

--- src/pages/AllAPIsPage/AllAPIsPage.js
+++ src/pages/AllAPIsPage/AllAPIsPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import styled from 'styled-components';
 
+import { Error } from "../../components/Error/Error";
+
 //import redux selectors, dispatch
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAll, isLoading, selectAllAPIs, isError, resetError } from "../../features/apiSlice";
@@ -57,56 +59,6 @@ const Link = styled.a`
     }
 `;
 
-const ErrorContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin: auto;
-    width: 100%;
-    height: calc(100vh - 70px - 28px);
-    justify-content: center;
-    align-items: center;
-`;
-
-const ErrorText = styled.h2`
-    font-family: 'Inter';
-    font-size: 16px;
-    font-weight: 600;
-    letter-spacing: .10em;
-    line-height: 1.16667;
-    border-radius: 5px;
-    color: red;
-`;
-
-const Button = styled.button`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 10px 0px;
-    background-color: #fff;
-    color: #48434f;
-    border: 1px solid #48434f;
-    font-family: 'Inter';
-    font-size: 12px;
-    font-weight: 600;
-    letter-spacing: .10em;
-    line-height: 1.16667;
-    text-decoration: none;
-    width: 220px;
-    text-align: center;
-    height: 39px;
-    padding: 0 28px;
-    cursor: pointer;
-    border-radius: 4px;
-    transition: .2s;
-    text-transform: uppercase;
-    -webkit-font-smoothing: antialiased;
-
-    &:hover{
-        background-color: #48434f;
-        color: #fff;
-    }
-`;
-
 export const AllAPIsPage = () => {
     const dispatch = useDispatch();
     const allAPIs = useSelector(selectAllAPIs);
@@ -123,14 +75,7 @@ export const AllAPIsPage = () => {
     };
 
     if(error) {
-        return (
-            <ErrorContainer>
-                <ErrorText>
-                    Error loading data
-                </ErrorText>
-                <Button onClick={handleClick}>Retry</Button>
-            </ErrorContainer>
-        )
+        return <Error handleClick={handleClick}/>
     };
 
     return (
