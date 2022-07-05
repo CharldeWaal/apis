860b3f3437fc2be24b0014f8c9a255b652540d42
Charl de Waal <charldewaal8@gmail.com>
2022-07-05 14:05:41 +0200

    Added utility for fetching API per category

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,6 +3,11 @@ export const fetchAllAPIs = async () => {
     return res.json();
 };
 
+export const fetchCategory = async (category) => {
+    const res = await fetch(`https://api.publicapis.org/entries?category=${category}`);
+    return res.json();
+};
+
 export const fetchAPICategories = async () => {
     const res = await fetch('https://api.publicapis.org/categories');
     return res.json();
