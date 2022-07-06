465cbadc8ce2d72226255e37cf6245b77addd104
Charl de Waal <charldewaal8@gmail.com>
2022-07-06 21:55:37 +0200

    Rolled back to pre heroku

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,19 +1,19 @@
 export const fetchAllAPIs = async () => {
-    const res = await fetch('https://api.publicapis.org/entries');
+    const res = await fetch('/entries');
     return res.json();
 };
 
 export const fetchCategory = async (category) => {
-    const res = await fetch(`https://api.publicapis.org/entries?category=${category}`);
+    const res = await fetch(`/entries?category=${category}`);
     return res.json();
 };
 
 export const fetchAPICategories = async () => {
-    const res = await fetch('https://api.publicapis.org/categories');
+    const res = await fetch('/categories');
     return res.json();
 };
 
 export const fetchRandomAPI = async () => {
-    const res = await fetch('https://api.publicapis.org/random');
+    const res = await fetch('/random');
     return res.json();
 }
