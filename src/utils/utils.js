505341c8aa9b8eba6940ed6b32b610da496228da
Charl de Waal <charldewaal8@gmail.com>
2022-07-06 21:48:43 +0200

    Removed proxy and changed fetch url for heroku test

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,19 +1,19 @@
 export const fetchAllAPIs = async () => {
-    const res = await fetch('/entries');
+    const res = await fetch('https://api.publicapis.org/entries');
     return res.json();
 };
 
 export const fetchCategory = async (category) => {
-    const res = await fetch(`/entries?category=${category}`);
+    const res = await fetch(`https://api.publicapis.org/entries?category=${category}`);
     return res.json();
 };
 
 export const fetchAPICategories = async () => {
-    const res = await fetch('/categories');
+    const res = await fetch('https://api.publicapis.org/categories');
     return res.json();
 };
 
 export const fetchRandomAPI = async () => {
-    const res = await fetch('/random');
+    const res = await fetch('https://api.publicapis.org/random');
     return res.json();
 }
